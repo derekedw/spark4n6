b422de9d9102ab6703065acf107dada991a340d6
Derek Edwards <derekedw@yahoo.com>
2015-10-04 10:15:38 +0000

    It works with pig

--- src/main/java/com/edwardsit/spark4n6/EWFRecordReader.java
+++ src/main/java/com/edwardsit/spark4n6/EWFRecordReader.java
@@ -10,19 +10,18 @@ import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.JobContext;
-import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileRecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.log4j.Logger;
 import org.apache.log4j.Level;
-import org.apache.spark.SparkConf;
 
 import java.io.IOException;
 
 /**
 * Created by Derek on 9/22/2014.
 */
-public class EWFRecordReader extends RecordReader<LongWritable, BytesWritable> {
+public class EWFRecordReader extends SequenceFileRecordReader<LongWritable, BytesWritable> {
     private static Logger log = Logger.getLogger(EWFRecordReader.class);
     private static long nChunksPerSplit = -1L;
     private long chunkSize = 0L;
@@ -93,13 +92,9 @@ public class EWFRecordReader extends RecordReader<LongWritable, BytesWritable> {
             stream.close();
     }
     protected long getChunksPerSplit() throws IOException {
-        SparkConf sparkConf = new SparkConf();
         if (nChunksPerSplit == -1L) {
-            // Use the smaller of Spark's blocksize or the HDFS filesystem's block size
-            // long sparkBlockSize = sparkConf.getSizeAsBytes("spark.broadcast.blockSize", "134217728");
-            long sparkBlockSize = sparkConf.getSizeAsBytes("spark.broadcast.blockSize", "1048576");
             long hdfsBlockSize = fs.getFileStatus(file).getBlockSize();
-            nChunksPerSplit = (Math.min(sparkBlockSize,hdfsBlockSize)/chunkSize) - 1L;
+            nChunksPerSplit = (hdfsBlockSize/chunkSize/8) - 1L;
         }
         return nChunksPerSplit;
     }
