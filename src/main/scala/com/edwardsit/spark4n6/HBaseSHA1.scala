0556114184a42e3e854ee0e1435d870c693a6f3e
Derek Edwards <derekedw@yahoo.com>
2015-10-12 05:14:15 -0400

    Completed an untested HBaseSHA1 implementation

--- src/main/scala/com/edwardsit/spark4n6/HBaseSHA1.scala
+++ src/main/scala/com/edwardsit/spark4n6/HBaseSHA1.scala
@@ -1,9 +1,11 @@
 package com.edwardsit.spark4n6
 
+import collection.JavaConversions._
 import org.apache.commons.codec.binary.Hex
-import org.apache.hadoop.hbase.client.HBaseAdmin
-import org.apache.hadoop.hbase.{HBaseConfiguration, HTableDescriptor}
-import org.apache.hadoop.hbase.mapreduce.TableInputFormat
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hbase.client._
+import org.apache.hadoop.hbase.filter.PrefixFilter
+import org.apache.hadoop.hbase.{HColumnDescriptor, HBaseConfiguration, HTableDescriptor}
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -15,42 +17,43 @@ object HBaseSHA1 {
     sha1.calculate
     println(args(0) + " = " + sha1.toHexString)
   }
+  val tableNameDefault: String = "images"
+  val familyNameDefault : String = "images"
 }
 
-class HBaseSHA1 (image: String, sparkMaster: String = "yarn-client", table: String = "images") {
+class HBaseSHA1 (image: java.lang.String, tableName: String = HBaseSHA1.tableNameDefault, familyName : String = HBaseSHA1.familyNameDefault) {
   val md = java.security.MessageDigest.getInstance("SHA1")
+  var calculated = false
+  def this(image: String) { this(image,HBaseSHA1.tableNameDefault,HBaseSHA1.familyNameDefault) }
   def toHexString : String = {
+    if (! calculated)
+      calculate()
     Hex.encodeHexString(md.digest())
   }
-  def calculate {
-    val sparkConf = new SparkConf().setAppName("HBaseTest")
-    val sc = new SparkContext(sparkMaster,"HBaseSHA1",sparkConf)
-
-    // please ensure HBASE_CONF_DIR is on classpath of spark driver
-    // e.g: set it through spark.driver.extraClassPath property
-    // in spark-defaults.conf or through --driver-class-path
-    // command line option of spark-submit
-
+  def calculate() {
     val conf = HBaseConfiguration.create()
-
-    // Other options for configuring scan behavior are available. More information available at
-    // http://hbase.apache.org/apidocs/org/apache/hadoop/hbase/mapreduce/TableInputFormat.html
-    conf.set(TableInputFormat.INPUT_TABLE, table)
-
     // Initialize hBase table if necessary
     val admin = new HBaseAdmin(conf)
-    if (!admin.isTableAvailable(table)) {
-      val tableDesc = new HTableDescriptor(table)
+    val connection = HConnectionManager.createConnection(new Configuration)
+    if (!admin.isTableAvailable(tableName)) {
+      val tableDesc = new HTableDescriptor("test")
       admin.createTable(tableDesc)
+      tableDesc.addFamily(new HColumnDescriptor(familyName.getBytes))
     }
-
-    val hBaseRDD = sc.newAPIHadoopRDD(conf, classOf[TableInputFormat],
-      classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable],
-      classOf[org.apache.hadoop.hbase.client.Result])
-
-    hBaseRDD.count()
-
-    sc.stop()
+    val table = connection.getTable(tableName.getBytes)
+    val scan = new Scan(image.getBytes,new PrefixFilter(image.getBytes()))
+    scan.addFamily(familyName.getBytes)
+    val rs = table.getScanner(scan)
+    for (r : Result <- rs) {
+      val families = r.getNoVersionMap()
+      for (columns <- families.values()) {
+        for (data <- columns.values()) {
+          md.update(data)
+        }
+      }
+    }
+    rs.close()  // always close the ResultScanner!
     admin.close()
+    calculated = true
   }
 }
