943fb3d177744424d7b1e72e9e4c1b55539ee409
Derek Edwards <derekedw@yahoo.com>
2016-03-05 22:06:11 -0500

    Bug fixes

--- bin/test.sh
+++ bin/test.sh
@@ -3,12 +3,12 @@
 set +x
 	. ~/spark4n6/bin/functions.sh
 	testImage=$1
-	bucket=$(echo ${testImage} | sed -e 's/^\(s3n:\/\/[-.0-9a-z][-.0-9a-z]*\).*$/\1/'
+	bucket=$(echo ${testImage} | sed -e 's/^\(s3n:\/\/[-.0-9a-z][-.0-9a-z]*\).*$/\1/')
 	testImagePath=$(echo ${testImage} | sed -e 's/s3n:\/\/[-.0-9a-z][-.0-9a-z]*//')
 	testImageDir=$(dirname ${testImagePath})
-	testImageBase=$(basename ${testImagePath})
+	testImageBase=$(basename ${testImagePath} | sed -e 's/\.E01//')
 	hadoop distcp \
-        $(bucket}${testImageDir}/${testImageBase}.E* \
+        ${bucket}${testImageDir}/${testImageBase}.E* \
         /user/hadoop
 	while [ ! -d ~/spark4n6/ ]; do sleep 10; done
 	while [ ! -f ~/spark4n6/target/scala-2.10/spark4n6_2.10-1.0.jar ]; do sleep 10; done
