f01ef84affcc02870543e85fcf8ce6bb4aa50cd5
Derek Edwards <derekedw@yahoo.com>
2016-03-05 22:04:42 -0500

    Need to use https for checkout, to avoid needing a password

--- bin/start.sh
+++ bin/start.sh
@@ -2,7 +2,7 @@
 
 sudo yum --assumeyes install git tmux
 wget https://downloads.typesafe.com/typesafe-activator/1.3.7/typesafe-activator-1.3.7.zip
-git clone git@github.com:derekedw/spark4n6.git spark4n6
+git clone https://github.com/derekedw/spark4n6.git spark4n6
 
 set +x
 export testImage=$1
