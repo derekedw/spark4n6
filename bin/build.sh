240f48cee57d34e88e45df92b022254c32f8add5
Derek Edwards <derekedw@yahoo.com>
2016-03-05 23:05:52 -0500

    Can not use tmux with the bootstrap program because there is no terminal

--- bin/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set +x
-	. ~/spark4n6/bin/functions.sh
-    unzip ~/typesafe-activator-1.3.7.zip
-    (   cd ~/spark4n6
-        activator package
-    )
-
--- bin/start.sh
+++ bin/start.sh
@@ -3,8 +3,13 @@
 sudo yum --assumeyes install git tmux
 wget https://downloads.typesafe.com/typesafe-activator/1.3.7/typesafe-activator-1.3.7.zip
 git clone https://github.com/derekedw/spark4n6.git spark4n6
+. ~/spark4n6/bin/functions.sh
+unzip ~/typesafe-activator-1.3.7.zip
+(   cd ~/spark4n6
+    activator package
+)
 
-set +x
-export testImage=$1
-tmux new-session "~/spark4n6/bin/test.sh ${testImage}"
-tmux split-window -h ~/spark4n6/bin/build.sh
+# set +x
+# export testImage=$1
+# tmux new-session "~/spark4n6/bin/test.sh ${testImage}"
+# tmux split-window -h ~/spark4n6/bin/build.sh
