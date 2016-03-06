5f2b5f4c91ae556dfee9e6680f50fb3dc34f7918
Derek Edwards <derekedw@yahoo.com>
2016-03-05 23:29:29 -0500

    Error sourcing functions.sh

--- bin/start.sh
+++ bin/start.sh
@@ -2,9 +2,10 @@
 
 sudo yum --assumeyes install git tmux
 wget https://downloads.typesafe.com/typesafe-activator/1.3.7/typesafe-activator-1.3.7.zip
-git clone https://github.com/derekedw/spark4n6.git spark4n6
-. ~/spark4n6/bin/functions.sh
 unzip ~/typesafe-activator-1.3.7.zip
+git clone https://github.com/derekedw/spark4n6.git spark4n6
+
+. ~/spark4n6/bin/functions.sh
 (   cd ~/spark4n6
     activator package
 )
