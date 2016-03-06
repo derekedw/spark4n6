304d65119311a6e504fb4dd96b9aa1fef80006d1
Derek Edwards <derekedw@yahoo.com>
2016-03-05 22:25:33 -0500

    Quotes are needed

--- bin/start.sh
+++ bin/start.sh
@@ -6,5 +6,5 @@ git clone https://github.com/derekedw/spark4n6.git spark4n6
 
 set +x
 export testImage=$1
-tmux new-session ~/spark4n6/bin/test.sh ${testImage}
+tmux new-session "~/spark4n6/bin/test.sh ${testImage}"
 tmux split-window -h ~/spark4n6/bin/build.sh
