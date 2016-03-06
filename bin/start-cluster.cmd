456786b55d7fc454a2a708d941714520e3f4c468
Derek Edwards <derekedw@yahoo.com>
2016-03-05 22:03:48 -0500

    Added HBase and a custom bootstrap action

--- bin/start-cluster.cmd
+++ bin/start-cluster.cmd
@@ -15,21 +15,22 @@ aws emr create-cluster ^
    --ami-version 3.11.0 ^
    --instance-groups ^
        Name="Spark4n6Master",InstanceCount=1,InstanceGroupType=MASTER,BidPrice=%spotBidPrice%,InstanceType=m1.xlarge ^
-       Name="Spark4n6 Core",InstanceCount=15,InstanceGroupType=CORE,%spotBidPrice%,InstanceType=m3.xlarge ^
+       Name="Spark4n6 Core",InstanceCount=15,InstanceGroupType=CORE,BidPrice=%spotBidPrice%,InstanceType=m3.xlarge ^
    --service-role EMR_DefaultRole ^
    --ec2-attributes KeyName=%keyPairName%,InstanceProfile=EMR_EC2_DefaultRole,SubnetId=%subnet% ^
    --log-uri s3://%bucket%/%logPath% ^
    --applications ^
-       Name=HADOOP ^
-       Name=PIG ^
-       Name=HUE ^
-       Name=SPARK ^
+           Name=HADOOP ^
+           Name=PIG ^
+           Name=HUE ^
+           Name=SPARK ^
+           Name=HBASE ^
+           Name=GANGLIA ^
    --bootstrap-actions ^
        Name="Install Ganglia",Path=s3://elasticmapreduce/bootstrap-actions/install-ganglia ^
        Name="Install Spark",Path=file:///usr/share/aws/emr/install-spark/install-spark ^
        Name="Install HBase",Path=s3://elasticmapreduce/bootstrap-actions/setup-hbase ^
        Name="Configure Hadoop",Args=["--hdfs-key-value","io.file.buffer.size=65536"],Path=s3://elasticmapreduce/bootstrap-actions/configure-hadoop ^
-       Name="Configure daemons",Args=["--namenode-opts=-XX:GCTimeRatio=19"],Path=s3://elasticmapreduce/bootstrap-actions/configure-daemons
-::
-::     Name="Initialize the cluster",Path=s3://elasticmapreduce/bootstrap-actions/run-if,Args=["instance.isMaster=true",s3://spark4n6-public/bootstrap-actions/start.sh,s3n://%bucket%/%testImagePath%]
+       Name="Configure daemons",Args=["--namenode-opts=-XX:GCTimeRatio=19"],Path=s3://elasticmapreduce/bootstrap-actions/configure-daemons ^
+       Name="Initialize the cluster",Path=s3://elasticmapreduce/bootstrap-actions/run-if,Args=["instance.isMaster=true",s3://spark4n6-public/bootstrap-actions/start.sh,s3n://%bucket%/%testImagePath%]
 
