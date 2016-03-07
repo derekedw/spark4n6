dfb03c64e5d42982c5c112bbba6f560e7cfc01c6
Derek Edwards <derekedw@yahoo.com>
2016-03-07 00:05:47 -0500

    This bootstrap action script was failing because it was not running in the environment of user hadoop

--- bin/start.sh
+++ bin/start.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 set -x
-sudo yum --assumeyes install git tmux
-wget https://downloads.typesafe.com/typesafe-activator/1.3.7/typesafe-activator-1.3.7.zip
-unzip ~/typesafe-activator-1.3.7.zip
-git clone https://github.com/derekedw/spark4n6.git spark4n6
+sudo -i -u hadoop sh -c "
+    wget https://downloads.typesafe.com/typesafe-activator/1.3.7/typesafe-activator-1.3.7.zip;
+    unzip ~/typesafe-activator-1.3.7.zip;
+    git clone https://github.com/derekedw/spark4n6.git spark4n6;
 
-. ~/spark4n6/bin/functions.sh
-(   cd ~/spark4n6
-    activator package
-)
+    . ~/spark4n6/bin/functions.sh;
+    (   cd ~/spark4n6;
+        activator package;
+    );
+"
 
 # set +x
 # export testImage=$1
